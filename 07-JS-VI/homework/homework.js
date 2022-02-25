8b11d297c4c62c4980c3363bc9a4edbb4cd1c167
maria candelaria mendez casariego <tu@email.com>
2022-02-25 15:50:17 -0300

    tarea 7

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -3,7 +3,8 @@
 function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
-  //Tu código:
+  //Tu código:}
+  no
 }
 
 function invocarCallback(cb) {
